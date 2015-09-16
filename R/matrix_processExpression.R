fbc5abcb323796bd4d2ba1c3f04292386fb56ec6
Kaur Alasoo <kaur.alasoo@gmail.com>
2015-09-16 20:40:06 +0100

    Find correlations betweeb covariates and PEER factors

--- R/matrix_processExpression.R
+++ R/matrix_processExpression.R
@@ -75,3 +75,28 @@ filterDESeqResults <- function(results,gene_metadata, min_padj = 0.01, min_fc = 
   }
   return(list(up_genes = up_genes, down_genes = down_genes, results_table = result_table))
 }
+
+
+explainPEER <- function(peer_factors, covariates){
+  #Identify which covariates are most correlated with hidden factors discovered by PEER
+  dataset = cbind(covariates, peer_factors)
+  covar_names = colnames(covariates)
+  factor_names = colnames(peer_factors)
+  results = c()
+  for (covar in covar_names){
+    for (factor in factor_names){
+      formula = as.formula(paste(factor, covar, sep = " ~ "))
+      model = lm(formula, data = dataset)
+      model_summary = summary(model)
+      result = c(covar, factor, model_summary$r.squared, model_summary$coefficients[2,4])
+      results = rbind(results, result)
+    }
+  }
+  colnames(results) = c("covariate", "peer_factor", "r_squared", "p_value")
+  rownames(results) = c()
+  results = as.data.frame(results, stringsAsFactors = FALSE)
+  results$r_squared = as.numeric(results$r_squared)
+  results$p_value = as.numeric(results$p_value)
+  return(results)
+}
+
