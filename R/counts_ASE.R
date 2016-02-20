fb7270aa8a57c4b69fb5f464ddedfac4cd6d72b0
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-02-20 19:53:50 +0000

    Make processing genotype data more efficient

--- R/counts_ASE.R
+++ R/counts_ASE.R
@@ -1,5 +1,7 @@
-constructExonRanges <- function(gene_data){
-  gene_df = data_frame(gene_id = gene_data$gene_id, 
+constructExonRanges <- function(selected_gene_id, lead_snp_id, gene_metadata){
+  gene_data = dplyr::filter(gene_metadata, gene_id == selected_gene_id)
+  gene_df = data_frame(gene_id = gene_data$gene_id,
+                       lead_snp_id = lead_snp_id,
                        seqnames = gene_data$chr, 
                        strand = gene_data$strand, 
                        start = as.numeric(unlist(strsplit(gene_data$exon_starts, ","))), 
@@ -28,7 +30,7 @@ tabixFetchASE <- function(ranges, tabix_file){
 
 rangesQueryMeta <- function(ranges){
   #Add metadata to the tabix import
-  df = as.data.frame(elementMetadata(ranges))
+  df = as.data.frame(GenomicRanges::elementMetadata(ranges))
   query_string = GenomicRanges::as.data.frame(ranges) %>% 
     tbl_df() %>% 
     dplyr::mutate(query = paste(paste(seqnames, start, sep = ":"), end, sep = "-")) %>% 
@@ -38,7 +40,7 @@ rangesQueryMeta <- function(ranges){
 }
 
 tidyASECounts <- function(ase_counts){
-  result = tidyr::gather(ase_counts, key = "sample_id", value = "ASE", 7:ncol(a)) %>% 
+  result = tidyr::gather(ase_counts, key = "sample_id", value = "ASE", 7:ncol(ase_counts)) %>% 
     tidyr::separate(ASE,c("ref_count", "alt_count"), sep = ",") %>%
     dplyr::mutate(ref_count = as.numeric(ref_count), alt_count = as.numeric(alt_count)) %>%
     dplyr::mutate(total_count = ref_count + alt_count, max_count = pmax(ref_count, alt_count)) %>%
@@ -55,23 +57,17 @@ addVariantGenotype <- function(variant_df, genotypes, snp_col = "snp_id", value_
 }
 
 #Fetch ASE data for a single gene
-fetchGeneASEData <- function(gene_snp_pair, tabix_file, genotypes, sample_metadata, gene_metadata){
-  assertthat::assert_that(nrow(gene_snp_pair) == 1) #Must contain only one gene and SNP
-  
-  gene_ranges = dplyr::filter(gene_metadata, gene_id == gene_snp_pair$gene_id) %>% constructExonRanges()
+
+fetchGeneASEData <- function(gene_ranges, tabix_file, sample_metadata){
+  assertthat::assert_that(class(gene_ranges)[1] == "GRanges")
+
   ase_data = tabixFetchASE(gene_ranges, tabix_file)
   if(nrow(ase_data) > 0){
-    ase_data = tidyASECounts(ase_data) %>%
-      dplyr::left_join(rangesQueryMeta(gene_ranges), by = "query") %>% dplyr::select(-query) %>% #Add gene id
-      dplyr::left_join(gene_snp_pair, by = "gene_id") %>% #Add lead SNP id
-      dplyr::left_join(sample_metadata, ., by = "sample_id") %>%
-      addVariantGenotype(genotypes, snp_col = "snp_id", value_id = "lead_snp")
-    
-    #Split by variant
-    variant_list = dlply(ase_data, .variables = "feature_snp_id")
-    variant_list = lapply(variant_list, addVariantGenotype, genotypes, snp_col = "feature_snp_id", value_id = "feature_snp")
-    variant_df = ldply(variant_list, .id = NULL)
-    return(variant_df)
+    ase_data = tidyASECounts(ase_data) %>% #Tidyfy
+      dplyr::left_join(rangesQueryMeta(gene_ranges), by = "query") %>% dplyr::select(-query) %>% #Add gene id and lead snp id
+      dplyr::mutate(sample_id = as.character(sample_id)) %>%
+      dplyr::left_join(sample_metadata, ., by = "sample_id") #Add sample metadata
+    return(ase_data)
   }else{
     return(ase_data)
   }
@@ -79,10 +75,30 @@ fetchGeneASEData <- function(gene_snp_pair, tabix_file, genotypes, sample_metada
 
 filterASEforPlotting <-function(ase_data){
   #Keep only hets
-  hets = dplyr::filter(ase_data, lead_snp == 1, feature_snp == 1)
+  hets = dplyr::filter(ase_data, feature_snp_value == 1)
   
   #Remove feature SNPs with very low coverage
-  expressed_snps = dplyr::group_by(hets, feature_snp_id) %>% dplyr::summarise(mean_total_count = mean(total_count)) %>% dplyr::filter(mean_total_count > 3)
+  expressed_snps = dplyr::group_by(hets, feature_snp_id) %>% dplyr::summarise(mean_total_count = mean(total_count)) %>% dplyr::filter(mean_total_count > 10)
   selected_hets = dplyr::semi_join(hets, expressed_snps, by = "feature_snp_id")
   return(selected_hets)
+}
+
+tidyGenotypeMatrix <- function(genotypes, selected_snp_id = "snp_id", value_id = "snp_value"){
+  rows = rownames(genotypes)
+  result = tbl_df(as.data.frame(genotypes)) %>% 
+    dplyr::mutate(snp_id = rows) %>% 
+    tidyr::gather_("genotype_id", value_id, 1:ncol(genotypes)) %>%
+    dplyr::mutate(genotype_id = as.character(genotype_id)) %>%
+    dplyr::rename_(.dots = setNames("snp_id", selected_snp_id))
+  return(result)
+}
+
+aseDataAddGenotypes <- function(ase_data, genotypes){
+  assertthat::not_empty(ase_data) #Make sure data is not empty
+  
+  ase_data_gt = dplyr::left_join(ase_data, tidyGenotypeMatrix(genotypes[unique(ase_data$feature_snp_id),,drop = FALSE], 
+        value_id = "feature_snp_value", selected_snp_id = "feature_snp_id"), by = c("feature_snp_id", "genotype_id")) %>%
+    dplyr::left_join(tidyGenotypeMatrix(genotypes[unique(ase_data$lead_snp_id),,drop = FALSE], 
+        value_id = "lead_snp_value", selected_snp_id = "lead_snp_id"), by = c("lead_snp_id", "genotype_id"))
+  return(ase_data_gt)
 }
