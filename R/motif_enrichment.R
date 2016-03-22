80cf603c13d9d878559a249e7b611ed753920808
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-03-22 15:37:09 +0200

    Import cisBP database into TFBSTools

--- R/motif_enrichment.R
+++ R/motif_enrichment.R
@@ -1,8 +1,54 @@
-
 cisBPImportMotif <- function(motif_id, cisbp_dir){
   motif_path = file.path(cisbp_dir, paste(motif_id, ".txt", sep = ""))
   motif = read.table(motif_path, header = TRUE)
   motif = t(motif[,-1])
   motif = round(motif*1000)
+  storage.mode(motif) = "integer"
   return(motif)
+}
+
+cisBPMotifToPFMatrix <- function(motif_record, cisbp_dir){
+  pfm = cisBPImportMotif(motif_record$Motif_ID, cisbp_dir)
+  if(ncol(pfm) == 0){
+    warning(paste("Skipping ", motif_record$Motif_ID, " because the PFM is empty.", sep = ""))
+    return(NULL)
+  }
+  pfmatrix = TFBSTools::PFMatrix(ID = motif_record$Motif_ID, name = motif_record$TF_Name,
+                      tags = list(gene_id = motif_record$gene_id),
+                      profileMatrix = pfm)
+  return(pfmatrix)
+}
+
+asPWMatrixList <- function(pwm_list){
+  result = TFBSTools::PWMatrixList()
+  for (i in seq_along(pwm_list)){
+    result[[i]] = pwm_list[[i]]
+  }
+  names(result) = names(pwm_list)
+  return(result)
+}
+
+cisBPImportRecords <- function(motifs_df, cisbp_dir){
+  
+  #Convert motifs df to list
+  motif_list = plyr::dlply(motifs_df, .(Motif_ID))
+
+  #Import PWMs
+  pwm_list = lapply(motif_list, function(motif_record, cisbp_dir) {
+    pfm = cisBPMotifToPFMatrix(motif_record, cisbp_dir)
+    if (!is.null(pfm)){
+      pwm = TFBSTools::toPWM(pfm)
+    } else{
+      pwm = NULL
+    }
+    return(pwm)
+  }, cisbp_dir)
+  
+  #Remove empty matrices
+  non_empty = which(!unlist(lapply(pwm_list, is.null)))
+  pwm_list = pwm_list[non_empty]
+  
+  #Convert to PWMatrixtList object
+  pwmatrix_list = asPWMatrixList(pwm_list)
+  return(pwmatrix_list)
 }
