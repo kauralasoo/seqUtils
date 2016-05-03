13e5f1fb5b7ad80b983cbd504a0fb78108a90fd2
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-05-03 18:10:52 +0100

    Add overlapping peaks to a QTL credible set

--- R/qtl_finemapping.R
+++ R/qtl_finemapping.R
@@ -9,21 +9,12 @@
 #' @export
 annotateCredibleSet <- function(eqtl_credible_set, atac_peak_metadata, atac_tabix_path){
   
-  #construct Granges object with atac peak data
-  peak_ranges = dataFrameToGRanges(dplyr::rename(atac_peak_metadata, seqnames = chr))
-  
   #Construct Granges for the credible set SNPs
   variant_ranges = dplyr::transmute(eqtl_credible_set, seqnames = chr, start = pos, end = pos, strand = "+", snp_id) %>%
     dataFrameToGRanges()
   
-  #Find overlapping peaks for each variant
-  olaps = GenomicRanges::findOverlaps(variant_ranges, peak_ranges, ignore.strand = TRUE)
-  variants_overlaps = variant_ranges[GenomicRanges::queryHits(olaps)]
-  peak_overlaps = peak_ranges[GenomicRanges::subjectHits(olaps),]
-  overlapping_peaks = dplyr::data_frame(snp_id = variants_overlaps$snp_id, overlap_peak_id = peak_overlaps$gene_id)
-  
   #Add overlapping peaks
-  peak_finemapping = dplyr::left_join(eqtl_credible_set, overlapping_peaks, by = "snp_id")
+  peak_finemapping = addOverlappingPeaks(eqtl_credible_set, atac_peak_metadata)
   
   #Find most associated peaks for each SNP
   associated_peaks = tabixFetchSNPs(variant_ranges, atac_tabix_path)
@@ -44,3 +35,52 @@ annotateCredibleSet <- function(eqtl_credible_set, atac_peak_metadata, atac_tabi
   return(peak_finemapping)
 }
 
+#' Add overlapping ATAC peaks to a credible set of QTL variants
+#'
+#' @param qtl_credible_set Data frame with credible set of QTL variants 
+#' (columns: snp_id, chr, pos)
+#' @param atac_peak_metadata Data frame of start end coordinates of ATAC peaks 
+#' (columns: gene_id, chr, start, end, strand)
+#' @param extend Number of bp added to peak start and end coordinates to increase the chance of finding an overlap.
+#'
+#' @return Modifed qtl_credible_set data frame with added overlap_peak_id column.
+#' @export
+addOverlappingPeaks <- function(qtl_credible_set, atac_peak_metadata, extend = 0) {
+  
+  #Some basic assertions
+  #Check that atac_peak_metadata has required columns
+  assertthat::assert_that(assertthat::has_name(atac_peak_metadata, "gene_id"))
+  assertthat::assert_that(assertthat::has_name(atac_peak_metadata, "chr"))
+  assertthat::assert_that(assertthat::has_name(atac_peak_metadata, "start"))
+  assertthat::assert_that(assertthat::has_name(atac_peak_metadata, "end"))
+  assertthat::assert_that(assertthat::has_name(atac_peak_metadata, "strand"))
+  
+  #Check that qtl_credible_set has required columns
+  assertthat::assert_that(assertthat::has_name(qtl_credible_set, "snp_id"))
+  assertthat::assert_that(assertthat::has_name(qtl_credible_set, "chr"))
+  assertthat::assert_that(assertthat::has_name(qtl_credible_set, "pos"))
+  
+  assertthat::assert_that(is.numeric(extend))
+  
+  #Extend peaks by some buffer sequence
+  if(extend > 0){
+    atac_peak_metadata = dplyr::mutate(atac_peak_metadata, start = start - extend, end = end + extend)
+  }
+  
+  #construct Granges object with atac peak data
+  peak_ranges = dataFrameToGRanges(dplyr::rename(atac_peak_metadata, seqnames = chr))
+  
+  #Construct Granges for the credible set SNPs
+  variant_ranges = dplyr::transmute(qtl_credible_set, seqnames = chr, start = pos, end = pos, strand = "+", snp_id) %>%
+    dataFrameToGRanges()
+  
+  #Find overlapping peaks for each variant
+  olaps = GenomicRanges::findOverlaps(variant_ranges, peak_ranges, ignore.strand = TRUE)
+  variants_overlaps = variant_ranges[GenomicRanges::queryHits(olaps)]
+  peak_overlaps = peak_ranges[GenomicRanges::subjectHits(olaps),]
+  overlapping_peaks = dplyr::data_frame(snp_id = variants_overlaps$snp_id, overlap_peak_id = peak_overlaps$gene_id)
+  
+  #Add overlapping peaks
+  peak_finemapping = dplyr::left_join(qtl_credible_set, overlapping_peaks, by = "snp_id")
+  return(peak_finemapping)
+}
