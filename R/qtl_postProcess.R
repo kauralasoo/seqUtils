bef2f56485b97cab7fa923d75b90c8abfb9c903c
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-09-10 16:43:38 +0300

    Specify feature id for PI1 test.

--- R/qtl_postProcess.R
+++ R/qtl_postProcess.R
@@ -8,11 +8,11 @@
 #' @return None
 #' @author Kaur Alasoo
 #' @export 
-calculatePi1 <- function(table1, table2, qvalue_thresh = 0.1){
+calculatePi1 <- function(table1, table2, qvalue_thresh = 0.1, feature_id = "gene_id"){
   #Identify significant hits from first table
   table1_hits = dplyr::filter(table1, qvalue < qvalue_thresh)
   #Extract the same genes from the second table
-  table2_hits = dplyr::semi_join(table2, table1_hits, by = "gene_id")
+  table2_hits = dplyr::semi_join(table2, table1_hits, by = feature_id)
   #Estimate the proportion of replicated qtls
   pi1 = 1 - qvalue::qvalue(table2_hits$p_beta)$pi0
   return(pi1)
@@ -27,7 +27,7 @@ calculatePi1 <- function(table1, table2, qvalue_thresh = 0.1){
 #' @return None
 #' @author Kaur Alasoo
 #' @export 
-calculatePairwisePi1 <- function(qtl_list, qvalue_thresh = 0.1, tidy = FALSE){
+calculatePairwisePi1 <- function(qtl_list, qvalue_thresh = 0.1, tidy = FALSE, feature_id = "gene_id"){
   sample_names = names(qtl_list)
   rep_matrix = matrix(1,length(sample_names),length(sample_names))
   colnames(rep_matrix) = sample_names
@@ -37,7 +37,7 @@ calculatePairwisePi1 <- function(qtl_list, qvalue_thresh = 0.1, tidy = FALSE){
   for (sn1 in 1:length(sample_names)){
     for (sn2 in 1:length(sample_names)){
       if (sn1 != sn2){
-        rep_matrix[sn1, sn2] = calculatePi1(qtl_list[[sn1]], qtl_list[[sn2]])
+        rep_matrix[sn1, sn2] = calculatePi1(qtl_list[[sn1]], qtl_list[[sn2]], qvalue_thresh, feature_id)
       }
     }
   }
