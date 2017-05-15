c6288ebc8286938715ead9bc09507f4eb61b668a
Kaur Alasoo <kaur.alasoo@gmail.com>
2017-05-15 15:55:49 +0100

    Visualise transcript ratios

--- R/qtl_visualise_trQTLs.R
+++ R/qtl_visualise_trQTLs.R
@@ -29,3 +29,28 @@ makeQTLCoveragePlot <- function(qtl_df, str1_df, str2_df, genotypes, exons, gene
   plot = wiggleplotr::plotCoverage(selected_exons, track_data = track_data, fill_palette = getGenotypePalette(), ...)
   return(plot)
 }
+
+extractTrQtlDataFromSE <- function(qtl_row, se, genotype_matrix, variant_information, 
+                                   condition_levels = c("Ctrl", "AcLDL"), phenotype_name = "tpm_ratios"){
+  #Extraxct sample metadata from SE
+  sample_metadata = SummarizedExperiment::colData(se) %>% tbl_df2() %>%
+    dplyr::mutate(condition_name = factor(condition_name, levels = condition_levels))
+  
+  #Extract phenotype matrix
+  pheno_matrix = SummarizedExperiment::assays(se)[[phenotype_name]]
+  
+  #Reformat gene data
+  transcript_metadata = tbl_df2(SummarizedExperiment::rowData(se)) %>% 
+    dplyr::mutate(gene_id = transcript_id, gene_name = transcript_id)
+  
+  #Construct data
+  data = constructQtlPlotDataFrame(qtl_row$phenotype_id, qtl_row$snp_id, 
+                                   pheno_matrix, genotype_matrix, 
+                                   sample_metadata, 
+                                   transcript_metadata) %>%
+    dplyr::left_join(constructGenotypeText(qtl_row$snp_id, variant_information), by = "genotype_value")
+  
+  return(data)
+}
+
+
